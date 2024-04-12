78d882bcfffcd0c2fec049628988928dfd18f746
sensaj <jafarsalum07@gmail.com>
2024-04-12 18:45:28 +0300

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/WeMovies",
-  plugins: [react()],
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",
+// });
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/WeMovies/";
+  }
+  return config;
 });
