ac82380fd5c6d7ad4750e3c0e8368035041ec7f2
sensaj <jafarsalum07@gmail.com>
2024-04-12 19:03:02 +0300

    Changed base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/WeMovies/",
+  base: "https://sensaj.github.io/WeMovies/",
 });
 
 // export default defineConfig(({ command }) => {
