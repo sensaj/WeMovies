b5276a3c76db7a433d6527229de0e023be28c2f1
sensaj <jafarsalum07@gmail.com>
2025-07-18 23:12:27 +0300

    Created new pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://sensaj.github.io/WeMovies/",
-  // base: "/",
+  // base: "https://sensaj.github.io/WeMovies/",
+  base: "/",
 });
